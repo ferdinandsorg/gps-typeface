968431203b3718250e7ef87aac554f2d239b1dda
Ferdinand Sorg <post@ferdinands.org>
2021-10-13 11:12:09 +0200

    change letters by clicking

--- map-geojson/script.js
+++ map-geojson/script.js
@@ -1,50 +1,61 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZmVyZGluYW5kc29yZyIsImEiOiJja3VvOHZleHAwczY0MnBvYXI0cHQ1eXZqIn0.Z8sROkSQ-Ovu-HERdbyeAg';
 const map = new mapboxgl.Map({
 	container: 'map',
-	style: 'mapbox://styles/ferdinandsorg/cktwz8bch198i18l9wfslqsi6',
+	// dark
+	style: 'mapbox://styles/ferdinandsorg/cji77ibe8231u2sqp2gwokaet',
+	// satellite
+	// style: 'mapbox://styles/ferdinandsorg/ckuokw3ia19oj17mrb6r9mtza',
 	center: [0,0],
 	zoom: 1
 });
 
-var geojson, bbox;
+var bbox, prevLetter;
 
-var letter = "g";
+function loadLetter(geojson, letter) {
 
-var url = "data/typeface-"+letter+".geojson";
+	// reset all Layer
+	if (prevLetter) {
+		map.removeLayer("letterOutline"+prevLetter);
+		map.removeSource("letterOutline"+prevLetter);
+	}
 
-$.getJSON(url, function(data) {
-	loadMap(data);
-});
+	var layerID = "letterOutline"+letter;
 
-function loadMap(geojson) {
+	map.addSource(layerID, {
+		"type": "geojson",
+		"data": geojson
+	});
 
-	bbox = turf.extent(geojson);
+	map.addLayer({
+		"id": layerID,
+		"type": "line",
+		"source": layerID,
+		"layout": {
+			'line-join': 'round',
+			'line-cap': 'round'
+		},
+		"paint": {
+			"line-color": "#B7EB74",
+			"line-width": 10
+		}
+	});
 
-	map.on('load', () => {
-		map.addSource('my-geojson', {
-	        "type": "geojson",
-	        "data": geojson
-	  });
+	prevLetter = letter;
+}
 
-		map.addLayer({
-	        "id": "my-geojson",
-	        "type": "line",
-	        "source": "my-geojson",
-					"layout": {
-	                'line-join': 'round',
-	                'line-cap': 'round'
-	        },
-	        "paint": {
-	            "line-color": "#B7EB74",
-							"line-width": 10
-	        }
-	    });
+function fit(letter) {
+	var url = "data/typeface-"+letter+".geojson";
+	$.getJSON(url, function(data) {
+
+		// load Letter into Map
+		loadLetter(data, letter);
+
+		// center map to Letter
+		bbox = turf.extent(data);
+		map.fitBounds(bbox, {padding: 40});
 	});
 }
 
-
-
-
-function fit() {
-	map.fitBounds(bbox, {padding: 40});
+function changeStyle() {
+	map.setStyle('mapbox://styles/ferdinandsorg/ckuokw3ia19oj17mrb6r9mtza');
 }
